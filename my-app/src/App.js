acff582958f728f08e4d86a1225ed4327eb25859
pierce-miller <114310142+pierce-miller@users.noreply.github.com>
2023-06-25 18:39:51 -0400

    Start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+  Grommet,
+  grommet,
+  Header,
+  Heading,
+  Page,
+  PageContent,
+  PageHeader,
+  Paragraph,
+  ResponsiveContext,
+  Text,
+} from "grommet";
+import { Moon, Sun } from "grommet-icons";
+import { deepMerge } from "grommet/utils";
+
+const theme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: "#228BE6",
+    },
+    font: {
+      family: "Poppins",
+      size: "18px",
+      height: "20px",
+    },
+  },
+});
+
+const AppBar = (props) => (
+  <Header
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    {...props}
+  />
+);
+
+const CardTemplate = ({ title }) => {
+  const size = useContext(ResponsiveContext);
+  return (
+    <Card>
+      <CardHeader pad="medium">
+        <Heading level={2} margin="none">
+          {title}
+        </Heading>
+      </CardHeader>
+      <CardBody pad="medium">
+        <Paragraph maxLines={size === "small" ? 3 : undefined}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Paragraph>
+      </CardBody>
+      <CardFooter pad="medium" background="background-contrast">
+        Footer
+      </CardFooter>
+    </Card>
+  );
+};
 
 function App() {
+  const [dark, setDark] = useState(false);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Grommet theme={theme} full themeMode={dark ? "dark" : "light"}>
+      <Page>
+        <AppBar>
+          <Text size="large">
+            My App
+          </Text>
+          <Button
+            icon={dark ? <Moon /> : <Sun />}
+            onClick={() => setDark(!dark)}
+            //inside Button attributes JSX => hover
+            tip={{
+              content: (
+                <Box
+                  pad="small"
+                  round="small"
+                  background={dark ? "dark-1" : "light-3"}
+                >
+                  {dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                </Box>
+              ),
+              plain: true,
+            }}
+          />
+        </AppBar>
+        <PageContent>
+          <PageHeader title="Welcome to Grommet" />
+          <Grid columns="medium" gap="large" pad={{ bottom: "large" }}>
+            <CardTemplate title={"Card1"}/>
+            <CardTemplate title={"Card1"}/>
+            <CardTemplate title={"Card1"}/>
+          </Grid>
+        </PageContent>
+      </Page>
+    </Grommet>
   );
 }
 

d28a296927a580b412cfebe8e6859e1c88092f68
pierce-miller <114310142+pierce-miller@users.noreply.github.com>
2023-07-28 14:50:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import {
   Paragraph,
   ResponsiveContext,
   Text,
+  Image,
 } from "grommet";
 import { Linkedin,Github } from "grommet-icons";
 import { deepMerge } from "grommet/utils";
@@ -55,18 +56,9 @@ const CardTemplate = ({ title }) => {
       </CardHeader>
       <CardBody pad="medium">
         <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Film Script Generator
         </Paragraph>
       </CardBody>
-      <CardFooter pad="medium" background="background-contrast">
-        Footer
-      </CardFooter>
     </Card>
   );
 };
@@ -115,6 +107,7 @@ function App() {
         <Box elevation="large" round="small" background={dark ? "dark-2" : "light-1"} pad="medium" align="center" overflow='auto'>
           <Grid rows="auto" gap="medium">
             <CardTemplate title="hello"/>
+            
           </Grid>
         </Box>
       </Grid>
